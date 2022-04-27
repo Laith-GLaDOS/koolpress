117299b31dc4875eadc574d5f511842712f6a3d0
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-04-28 01:03:24 +0300

    eyy releasing it

--- README.md
+++ README.md
@@ -9,7 +9,5 @@ Not available yet
  - Disables `x-powered-by` header by default  
  - Already `.use()`s `express.json()` for out-of-the-box request body JSON deserialisation  
  - Access to the Express server object  
+ - Cool `middleware()` function!
  - Probably more coming!  
-  
-# Notes:  
- - The framework is in development right now, it will exit beta once the first NPM package is posted (named `koolpress`)  
---
+++ koolpress.mjs
@@ -0,0 +1,47 @@
+import express from 'express';
+const server = express();
+server.use(express.json());
+server.disable('x-powered-by');
+
+const req_methods_o = {
+    GET: 1,
+    HEAD: 2,
+    POST: 3,
+    PUT: 4,
+    DELETE: 5,
+    CONNECT: 6,
+    OPTIONS: 7,
+    TRACE: 8,
+    PATCH: 9
+};
+
+function middleware_f(handler) {
+    server.use((req, res, next) => {
+        handler(req, res);
+        next();
+    });
+}
+
+function add_routes_f(routes) {
+    for (let i = 0; i < routes.length; i++) {
+        if (routes[i].method = req_methods_o.GET)         server.get(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.HEAD)       server.head(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.POST)       server.post(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.PUT)         server.put(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.DELETE)   server.delete(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.CONNECT) server.connect(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.OPTIONS) server.options(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.TRACE)     server.trace(routes[i].path, routes[i].handler);
+        if (routes[i].method = req_methods_o.PATCH)     server.patch(routes[i].path, routes[i].handler);
+    }
+}
+
+function start_listening_f(port, start_msg) { server.listen(port, () => console.log(start_msg)); }
+
+export default {
+    express_server: server,
+    middleware: middleware_f,
+    req_methods: req_methods_o,
+    add_routes: add_routes_f,
+    start_listening: start_listening_f
+};
